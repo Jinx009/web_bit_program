498c68666c3146c9a73204fc2aa4fe66a7be5d0e
Jinx <jinxlovejinx@vip.qq.com>
2016-09-08 14:36:33 +0800

    js获取浏览器版本以及操作系统版本

--- checkAgents.js
+++ checkAgents.js
@@ -2,10 +2,19 @@ var Agents = new Array("Android", "iPhone", "SymbianOS", "Windows Phone", "iPad"
 var flag = true;
 
 /**
+ * 初始化加载程序
+ */
+window.onload = function(){
+    console.log(isMobile());
+    console.log(isWechat());
+    console.log(getOsVersion());
+}
+
+/**
  * 判断是不是移动端
  * @returns {boolean}
  */
-function isPC() {
+function isMobile() {
     var userAgentInfo = navigator.userAgent;
 
     for (var v = 0; v < Agents.length; v++) {
@@ -16,6 +25,7 @@ function isPC() {
     }
     return flag;
 }
+
 /**
  * 判断是不是微信浏览器
  * @returns {boolean}
@@ -27,4 +37,28 @@ function isWechat() {
         return true;
     else
         return false;
+}
+
+/**
+ * 判断浏览器所在机器操作系统版本
+ */
+function getOsVersion(){
+    var u = navigator.userAgent,version = '';
+    if (u.indexOf('Mac OS X') > -1) {
+        //ios
+        var regStr_saf = /OS [\d._]*/gi;
+        var verinfo = u.match(regStr_saf);
+        version = (verinfo + "").replace(/[^0-9|_.]/ig,'').replace(/_/ig,'.');
+    } else if (u.indexOf('Android') > -1
+        || u.indexOf('Linux') > -1) {
+        //android
+        version = u.substr(u.indexOf('Android') + 8, u.indexOf(";", u.indexOf("Android")) - u.indexOf('Android') - 8);
+    } else if (u.indexOf('BB10') > -1) {
+        //黑莓bb10系统
+        version = u.substr(u.indexOf('BB10') + 5, u.indexOf(";", u.indexOf("BB10")) - u.indexOf('BB10') - 5);
+    } else if (u.indexOf('IEMobile')) {
+        //windows phone
+        version = u.substr(u.indexOf('IEMobile') + 9, u.indexOf(";", u.indexOf("IEMobile")) - u.indexOf('IEMobile') - 9);
+    }
+    return version;
 }
