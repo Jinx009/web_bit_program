7ae197fe21a2a033ee289cc36a43b5ae48d44345
Jinx <jinxlovejinx@vip.qq.com>
2016-09-29 10:37:01 +0800

    兼容19位银行卡号校验

--- bankCardCheck.js
+++ bankCardCheck.js
@@ -3,26 +3,36 @@
  */
 function checkCard(bankCardNum){
     bankCardNum = $('#card_no').val();
-    var sumOdd = 0;
-    var sumEven = 0;
+    var sumOdd = 0,sumEven = 0;
     if(bankCardNum==null||''==bankCardNum)
         return "银行卡号不能为空!";
 
-    var length = bankCardNum.length;
-    var  wei = new Array(length);
-    for (var i = 0; i < length; i++) {
-        wei[i] = parseInt(bankCardNum.substring(length - i - 1, length- i));// 从最末一位开始提取，每一位上的数值
+    var length = bankCardNum.length, lastNum = bankCardNum.substr(length-1,1),temp = 0;
+    bankCardNum = bankCardNum.substr(0,length-1);
+    for (var i = length-1; i >=1; i--) {
+        temp = parseInt(bankCardNum.substr(i-1,1));// 从末位一位开始提取，每一位上的数值
+        if(length%2==1){
+            if(i%2==0){
+                temp = 2*temp;
+                if(temp>10)
+                    temp = temp - 9;
+                sumEven += temp;
+            } else{
+                sumOdd += temp;
+            }
+        }else{
+            if(i%2==1){
+                temp = 2*temp;
+                if(temp>10)
+                    temp = temp - 9;
+                sumEven += temp;
+            } else{
+                sumOdd += temp;
+            }
+        }
     }
-    for (var i = 0; i < length / 2; i++) {
-        sumOdd += wei[2 * i];
-        if ((wei[2 * i + 1] * 2) > 9)
-            wei[2 * i + 1] = wei[2 * i + 1] * 2 - 9;
-        else
-            wei[2 * i + 1] *= 2;
-
-        sumEven += wei[2 * i + 1];
-    }
-    if ((sumOdd + sumEven) % 10 == 0){
+    console.log(sumOdd + sumEven+parseInt(lastNum));
+    if ((sumOdd + sumEven+parseInt(lastNum)) % 10 == 0){
         console.log('r')
         return "success";
     }
